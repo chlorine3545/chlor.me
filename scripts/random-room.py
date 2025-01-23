182575bfbbc7a67b1cdd480856c3dfc21cfc232e
Chlorine <git@chlor.me>
2025-01-23 22:07:27 +0800

    chore: add grtsinry43 friendlink

---
+++ scripts/random-room.py
@@ -0,0 +1,12 @@
+# 用来随机分配房间号的小东西 (/ω＼)
+import random
+
+
+def generate_room():
+    floor = str(random.randint(1, 14)).zfill(2)  # 01-14
+    room = str(random.randint(1, 30)).zfill(2)  # 01-30
+    wing = random.choice(["A", "B"])  # A or B
+    return f"{floor}{room}{wing}"
+
+
+print(generate_room())
