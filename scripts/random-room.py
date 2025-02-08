6a89b3cbded25182174e0ba7643447e5c58f3b93
Chlorine <dev@chlor.me>
2025-02-08 18:00:31 +0800

    chore: update random-room.py

--- scripts/random-room.py
+++ scripts/random-room.py
@@ -4,7 +4,7 @@ import random
 
 def generate_room():
     floor = str(random.randint(1, 14)).zfill(2)  # 01-14
-    room = str(random.randint(1, 35)).zfill(2)  # 01-35
+    room = str(random.randint(1, 99)).zfill(2)  # 01-99
     wing = random.choice(["A", "B"])  # A or B
     return f"{floor}{room}{wing}"
 
